51fb4a4c03701de46bdc080da315ed0c6ef2c4ab
Martastain <martas@imm.cz>
2016-06-19 12:18:43 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # rex
 yet another package manager
+
+
+Usage
+-----
+Rex has no special requirements. To use in your project you can download it using
+
+```wget imm.cz/rex.py```
---
+++ rex.py
@@ -0,0 +1,184 @@
+import os
+import sys
+import subprocess
+import time
+import json
+import traceback
+
+__all__ = [] # do not import anything
+
+DEBUG     = 0
+INFO      = 1
+WARNING   = 2
+ERROR     = 3
+GOOD_NEWS = 4
+
+PLATFORM = "windows" if sys.platform == "win32" else "unix"
+
+def indent(src, l=4):
+    return "\n".join(["{}{}".format(l*" ", s.rstrip()) for s in src.split("\n")])
+
+class Logging():
+    def __init__(self, user="REX"):
+        self.user = user
+        self.handlers = []
+        self.formats = {
+            INFO      : "INFO       {0:<15} {1}",
+            DEBUG     : "\033[34mDEBUG      {0:<15} {1}\033[0m",
+            WARNING   : "\033[33mWARNING\033[0m    {0:<15} {1}",
+            ERROR     : "\033[31mERROR\033[0m      {0:<15} {1}",
+            GOOD_NEWS : "\033[32mGOOD NEWS\033[0m  {0:<15} {1}"
+            }
+
+        self.formats_win = {
+            DEBUG     : "DEBUG      {0:<10} {1}",
+            INFO      : "INFO       {0:<10} {1}",
+            WARNING   : "WARNING    {0:<10} {1}",
+            ERROR     : "ERROR      {0:<10} {1}",
+            GOOD_NEWS : "GOOD NEWS  {0:<10} {1}"
+            }
+
+    def add_handler(self, handler):
+        self.handlers.append(handler)
+
+    def _send(self, msgtype, *args, **kwargs):
+        message = " ".join([str(arg) for arg in args])
+        user = kwargs.get("user", self.user)
+        if kwargs.get("handlers", True):
+            for handler in self.handlers:
+                handler(user=self.user, message_type=msgtype, message=message)
+        if PLATFORM == "unix":
+            try:
+                print (self.formats[msgtype].format(user, message))
+            except:
+                print (message.encode("utf-8"))
+        else:
+            try:
+                print (self.formats_win[msgtype].format(user, message))
+            except:
+                print (message.encode("utf-8"))
+
+    def debug(self, *args, **kwargs):
+        self._send(DEBUG, *args, **kwargs)
+
+    def info(self, *args, **kwargs):
+        self._send(INFO, *args, **kwargs)
+
+    def warning(self, *args, **kwargs):
+        self._send(WARNING, *args, **kwargs)
+
+    def error(self, *args, **kwargs):
+        self._send(ERROR, *args, **kwargs)
+
+    def goodnews(self, *args, **kwargs):
+        self._send(GOOD_NEWS, *args, **kwargs)
+
+
+logging = Logging()
+
+
+def log_traceback(message="Exception!", **kwargs):
+    tb = traceback.format_exc()
+    msg = "{}\n\n{}".format(message, indent(tb))
+    logging.error(msg, **kwargs)
+    return msg
+
+
+def critical_error(msg, **kwargs):
+    logging.error(msg, **kwargs)
+    logging.debug("Critical error. Terminating program.")
+    sys.exit(1)
+
+
+
+
+
+class Repository():
+    def __init__(self, parent,  url, **kwargs):
+        self.parent = parent
+        self.url = url
+        self.settings = kwargs
+        self.base_name = os.path.basename(url)
+        self.path = os.path.join(self.parent.vendor_dir, self.base_name)
+
+    def get(self, key, default=None):
+        return self.settings.get(key, default)
+
+    def __getitem__(self, key):
+        return self.settings[key]
+
+
+
+
+class Rex():
+    def __init__(self):
+        self.app_dir = os.path.dirname(sys.argv[0])
+        self.vendor_dir = os.path.join(self.app_dir, "vendor")
+        self.manifest_path = os.path.join(self.app_dir, "rex.json")
+
+        self.main()
+
+    @property
+    def force_update(self):
+        return "--rex-update" in sys.argv
+
+    @property
+    def repos(self):
+        if hasattr(self, "_repos"):
+            return self._repos
+        if not os.path.exists(self.manifest_path):
+            return []
+        try:
+            self.manifest = json.load(open(self.manifest_path))
+            if not self.manifest:
+                return []
+            self._repos = []
+            for repo_url in self.manifest.keys():
+                repo_settings = self.manifest[repo_url]
+                repo = Repository(self, repo_url, **repo_settings)
+                self._repos.append(repo)
+        except:
+            log_traceback()
+            return []
+        else:
+            return self._repos
+
+    def main(self):
+        for repo in self.repos:
+            try:
+                self.update(repo) and self.post_install(repo)
+            except:
+                log_traceback()
+        os.chdir(self.app_dir)
+
+
+
+    def update(self, repo):
+        if not os.path.exists(self.vendor_dir):
+            os.makedirs(self.vendor_dir)
+
+        if os.path.exists(repo.path):
+            if self.force_update:
+                os.chdir(repo.path)
+                cmd = ["git", "pull"]
+            else:
+                return True
+        else:
+            os.chdir(self.vendor_dir)
+            cmd = ["git", "clone", repo.url]
+
+        p = subprocess.Popen(cmd)
+        while p.poll() == None:
+            time.sleep(.1)
+        print "ret_code", p.returncode
+
+        return True
+
+    def post_install(self, repo):
+        if repo.get("python-path") and not repo.path in sys.path:
+            sys.path.insert(0, repo.path)
+
+
+
+rex = Rex()
+
