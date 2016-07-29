1c5475efd8d75625a3a077aef5b8512aa1afd40f
Martastain <martas@imm.cz>
2016-07-29 21:08:48 +0200

    clean-up

--- rex.py
+++ rex.py
@@ -73,9 +73,7 @@ class Logging():
     def goodnews(self, *args, **kwargs):
         self._send(GOOD_NEWS, *args, **kwargs)
 
-
 logging = Logging()
-
 
 def log_traceback(message="Exception!", **kwargs):
     tb = traceback.format_exc()
@@ -115,7 +113,7 @@ class Rex():
         self.app_dir = os.path.dirname(sys.argv[0])
         self.vendor_dir = os.path.join(self.app_dir, "vendor")
         self.manifest_path = os.path.join(self.app_dir, "rex.json")
-
+        self.self_update()
         self.main()
 
     @property
@@ -124,24 +122,38 @@ class Rex():
 
     @property
     def repos(self):
-        if hasattr(self, "_repos"):
-            return self._repos
-        if not os.path.exists(self.manifest_path):
-            return []
-        try:
-            self.manifest = json.load(open(self.manifest_path))
-            if not self.manifest:
-                return []
-            self._repos = []
-            for repo_url in self.manifest.keys():
-                repo_settings = self.manifest[repo_url]
-                repo = Repository(self, repo_url, **repo_settings)
-                self._repos.append(repo)
-        except:
-            log_traceback()
-            return []
+        if not hasattr(self, "_repos"):
+            if not os.path.exists(self.manifest_path):
+                self._repos = []
+            try:
+                self.manifest = json.load(open(self.manifest_path))
+                if not self.manifest:
+                    return []
+                self._repos = []
+                for repo_url in self.manifest.keys():
+                    repo_settings = self.manifest[repo_url]
+                    repo = Repository(self, repo_url, **repo_settings)
+                    self._repos.append(repo)
+            except:
+                log_traceback()
+                self._repos = []
+        return self._repos
+
+    def self_update(self):
+        if not self.force_update:
+            return
+        #if not os.path.exists(".rex_devel"):
+        #    logging.debug("This is a development machine. Skipping rex auto update.")
+        #    return
+        import urllib2
+        response = urllib2.urlopen("https://imm.cz/rex.py")
+        new_rex = response.read()
+        old_rex = open("rex.py").read()
+        if new_rex != old_rex:
+            logging.info("Updating REX")
         else:
-            return self._repos
+            logging.info("REX is up to date")
+
 
     def main(self):
         for repo in self.repos:
@@ -150,8 +162,6 @@ class Rex():
             except:
                 log_traceback()
         os.chdir(self.app_dir)
-
-
 
     def update(self, repo):
         if not os.path.exists(self.vendor_dir):
